edc14cf5d3662b2d89a34a5b93d81499c9bdfb85
ruminel <67764433+ruminel@users.noreply.github.com>
2024-03-19 20:07:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ let productRemarks = new Map([
 	['639011027851',{skuid:'639011027851',remarks:'棋盘和象棋组合'}],
 	['661701782313',{skuid:'661701782313',remarks:'盒子易碎，让商家包装好些'}],
 	['714593945229',{firstOrder:'S_31207270',remarks:'需要留意价格'}],
-	['abc',{firstOrder:'S_31207270',remarks:'abc'}],
 ])
