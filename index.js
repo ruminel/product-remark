37909fe6611e0b7135b86dc88b1888615d60b110
ruminel <67764433+ruminel@users.noreply.github.com>
2024-03-19 17:07:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ let productRemarks = new Map([
 	['639011027851',{skuid:'639011027851',remarks:'需加收组合费'}],
 	['661701782313',{skuid:'661701782313',remarks:'盒子易碎，让商家包装好些}],
 	['123',{skuid:'123',remarks:'测速'}],
-	['abc',{skuid:'abc',remarks:'abc'}],
 ])
