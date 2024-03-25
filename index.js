99190ba7e12891b9e808780498912a1e9134735c
ruminel <67764433+ruminel@users.noreply.github.com>
2024-03-25 15:15:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ let productRemarks = new Map([
 	['661701782313',{skuid:'661701782313',remarks:'盒子易碎，让商家包装好些'}],
 	['714593945229',{firstOrder:'S_31207270',remarks:'需要留意价格'}],
 	['622422612181',{firstOrder:'F_84680342',remarks:'假发和发网组合'}],
-	['593903797625',{firstOrder:'O_31548708',remakrs:'要让客户确认logo方向'}]
+	['593903797625',{firstOrder:'O_31548708',remakrs:'要让客户确认logo方向'}],
+	['714805035914',{firstOrder:'S_29630056',remakrs:'确认食品届'}],
 ])
