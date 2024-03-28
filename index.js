02942c2b65ff0b155eb3d6967862ae5ddd373edc
ruminel <67764433+ruminel@users.noreply.github.com>
2024-03-28 16:22:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ let productRemarks = new Map([
 	['622422612181',{firstOrder:'F_84680342',remarks:'假发和发网组合'}],
 	['593903797625',{firstOrder:'O_31548708',remarks:'要让客户确认logo方向'}],
 	['714805035914',{firstOrder:'S_29630056',remarks:'确认食品届'}],
-	['726628787343',{firstOrder:'F_35261019',remarks:'多买5双备用'}],
+	['726628787343',{firstOrder:'F_35261019',remarks:'多买几双备用'}],
 ])
