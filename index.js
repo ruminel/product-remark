e5ba84644540e98f3d6eca779e9d2aa3a9fbccc0
ruminel <67764433+ruminel@users.noreply.github.com>
2024-04-19 15:36:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let productRemarks = new Map([
     ['593903797625', { firstOrder: 'O_31548708', remarks: '要让客户确认logo方向' }],
     ['714805035914', { firstOrder: 'S_29630056', remarks: '确认食品届' }],
     ['726628787343', { firstOrder: 'F_35261019', remarks: '多买几双备用' }],
-    ['679283023657', { firstOrder: 'S_74790820', remarks: '确认拉链的形状' }],
+    ['679283023657', { firstOrder: 'S_74790820', remarks: '确认拉链末端的形状' }],
     ['660387098410', { firstOrder: 'S_87530396', remarks: '不要发电池' }],
     ['728222535497', { firstOrder: 'F_05670437', remarks: '确认是否3个1组发货' }],
     ['724950195642', { firstOrder: 'F_88715291', remarks: '包装上有3+标志' }],
