71181fb1695bbd0f0db6a85d3014d1919a055142
ruminel <67764433+ruminel@users.noreply.github.com>
2024-08-28 10:20:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ let productRemarks = new Map([
     ['747491997600', { firstOrder: '', remarks: '刀刃锋利，小心割手' }],
     ['787791191537', { firstOrder: 'J_02040104', remarks: '请店家检查好再发货' }],
     ['651133735200', { firstOrder: '', remarks: '客户的国内送料确认' }],
+    ['676321330185', { firstOrder: 'S_61975803', remarks: '商品附有胶水' }],
 ]);
 
 let fbaJsonreplenish = {
